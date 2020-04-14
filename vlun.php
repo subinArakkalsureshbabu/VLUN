ce43877800909a222c6d3b365cb0dfa105cf47a0
subinArakkalsureshbabu <subinarakkalsuresh@gmail.com>
2020-04-14 23:59:52 +0100

    Create vlun.php

---
+++ vlun.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>VLUN shop</title>
+    <link rel="stylesheet" href="css/bootstrap.css"/>
+    <link rel="stylesheet" href="css/main.css"/>
+    <meta name="viewport" content="width=device-width,initial scale=1,user-scale=no">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js">
+    </script>
+    <script src="js/bootstrap.min.js"></script>
+    </head>
+    <body>    
+    <nav class="navbar navbar-default navbar-fixed-top" id="navbar">
+      <div class ="container">
+          <a href="my project/vlun.php" class="navbar-brand" id="text">VLUN shop</a>
+          <ul class="nav navbar-nav">
+              
+          <li class ="dropdown">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown" id="text">Men<span class="caret"></span></a>
+            <ul class="dropdown-menu" role="menu">
+              <li><a href="#">shirts</a></li>
+              <li><a href="#">pants</a></li>
+              <li><a href="#">shoes</a></li>
+              <li><a href="#">Accessories</a></li>
+           </ul> 
+          </li>
+         </ul>
+        </div>  
+       </nav> 
+        
+        <div id="background-image">
+            <div id="image-1"></div>
+            <div id="image-2"></div>
+        </div>
+        <p>
+        wwwwwwwww ecommerce</p>
+      </body>
+    </html>
+ 
