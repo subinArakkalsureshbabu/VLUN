6a3bdc2c71d635afe37c55a42b7e13f8cca6614c
subinArakkalsureshbabu <subinarakkalsuresh@gmail.com>
2020-04-15 03:43:38 +0100

    Update vlun.php

--- vlun.php
+++ vlun.php
@@ -18,7 +18,7 @@
           <li class ="dropdown">
               <a href="#" class="dropdown-toggle" data-toggle="dropdown" id="text">Men<span class="caret"></span></a>
             <ul class="dropdown-menu" role="menu">
-              <li><a href="#">shirts</a></li>
+              < `li><a href="#">shirts</a></li>
               <li><a href="#">pants</a></li>
               <li><a href="#">shoes</a></li>
               <li><a href="#">Accessories</a></li>
@@ -32,8 +32,50 @@
             <div id="image-1"></div>
             <div id="image-2"></div>
         </div>
-        <p>
-        wwwwwwwww ecommerce</p>
+        
+        
+        <div class="col-md-2"></div>
+        
+        <div class="col-md-8">
+         <div class="row">
+        <h2 class="text-center">Products</h2>
+        <div class="col-md-3">
+            <h4>BLAZER</h4>
+            <img src="images/d.jpg" alt="blazer" id="images"/>
+            <p class="list-price text-danger">list price: <s>$24.99</s></p>
+            <p class="price">our price: $19.99</p>
+            <button type="button" class="btn btn-success" data-toggle="modal" data-target="details-1">Details</button>
+        </div>
+             
+             
+         <div class="col-md-3">
+            <h4>Formal</h4>
+            <img src="images/e.jpg" alt="sack-formal" id="images"/>
+            <p class="list-price text-danger">list price: <s>$24.99</s></p>
+            <p class="price">our price: $19.99</p>
+            <button type="button" class="btn btn-success" data-toggle="modal" data-target="details-1">Details</button>
+        </div>
+             
+             <div class="col-md-3">
+            <h4>party</h4>
+            <img src="images/f.jpg" alt="party wear" id="images"/>
+            <p class="list-price text-danger">list price: <s>$24.99</s></p>
+            <p class="price">our price: $19.99</p>
+            <button type="button" class="btn btn-success" data-toggle="modal" data-target="details-1">Details</button>
+        </div> 
+            
+             <div class="col-md-3">
+            <h4>sack</h4>
+            <img src="images/g.jpg" alt="sack" id="images"/>
+            <p class="list-price text-danger">list price: <s>$24.99</s></p>
+            <p class="price">our price: $19.99</p>
+            <button type="button" class="btn btn-success" data-toggle="modal" data-target="details-1">Details</button>
+        </div>
+        </div>
+        
+        <footer class="text-center" id="footer">&copy;Copyright 2020 VLUN The Blazer shop</footer>
+        </div> 
+        
       </body>
     </html>
  
