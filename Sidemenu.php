390e302c38d4053b9bf8ac3803f3494ea69822f2
subinArakkalsureshbabu <subinarakkalsuresh@gmail.com>
2020-04-17 22:22:03 +0100

    Create Sidemenu.php

---
+++ Sidemenu.php
@@ -0,0 +1,22 @@
+<div class="panel panel-default sidebar-menu">
+    <div class="panel-heading">
+        <h3 class="panel-title">Our products</h3>
+    </div>
+    <div class="panel-body">
+        <ul class="nav nav-pills nav-stacked category-menu">
+            <li><a href="#">BLAZERS</a></li>
+            <li><a href="#">ACCESSORIES</a></li>
+            </ul>
+    </div>
+</div>
+<div class="panel panel-default sidebar-menu">
+    <div class="panel-heading">
+        <h3 class="panel-title">Categories</h3>
+    </div>
+    <div class="panel-body">
+        <ul class="nav nav-pills nav-stacked category-menu">
+            <li><a href="#">Men</a></li>
+            <li><a href="#">Women</a></li>
+        </ul>
+    </div>
+</div>
