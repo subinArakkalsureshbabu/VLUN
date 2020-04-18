e3f25846be41ea8ddc13347413d8778a5bded0de
subinArakkalsureshbabu <subinarakkalsuresh@gmail.com>
2020-04-18 15:41:14 +0100

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,78 @@
+<div id="footer"><!-- #footer Begin -->
+    <div class="container"><!-- container Begin -->
+        <div class="row"><!-- row Begin -->
+            <div class="col-sm-6 col-md-3"><!-- col-sm-6 col-md-3 Begin -->
+               
+               <h4>Pages</h4>
+                
+                <ul><!-- ul Begin -->
+                    <li><a href="cart.php">Shopping Cart</a></li>
+                    <li><a href="contact.php">Contact Us</a></li>
+                    <li><a href="shop.php">Shop</a></li>
+                    <li><a href="checkout.php">My Account</a></li>
+                </ul><!-- ul Finish -->
+                
+                <hr>
+                
+                <h4>User Section</h4>
+                
+                <ul><!-- ul Begin -->
+                    <li><a href="checkout.php">Login</a></li>
+                    <li><a href="customer_register.php">Register</a></li>
+                </ul><!-- ul Finish -->
+                
+                <hr class="hidden-md hidden-lg hidden-sm">
+                
+            </div><!-- col-sm-6 col-md-3 Finish -->
+            
+            <div class="com-sm-6 col-md-3"><!-- col-sm-6 col-md-3 Begin -->
+                
+              
+                
+                <p><!-- p Start -->
+                    
+                    <strong>VLUN store</strong>
+                    <br/>Dublin
+                    <br/>Ireland
+                    <br/>VLUN.com
+                    
+                    
+                </p><!-- p Finish -->
+                
+                <a href="contact.php">please check our contact</a>
+                
+                <hr class="hidden-md hidden-lg">
+                
+            </div><!-- col-sm-6 col-md-3 Finish -->
+            
+            <div class="col-sm-6 col-md-3">
+                
+               
+                        
+                <hr>
+                
+                <h4>Keep In Touch</h4>
+                
+                <p class="social">
+                    <a href="#" class="fa fa-facebook"></a>
+                    
+                
+            </div>
+        </div><!-- row Finish -->
+    </div><!-- container Finish -->
+</div><!-- #footer Finish -->
+
+
+<div id="copyright"><!-- #copyright Begin -->
+    <div class="container"><!-- container Begin -->
+        <div class="col-md-6"><!-- col-md-6 Begin -->
+            
+            <p class="pull-left">&copy; 2020 VLUN shop</p>
+            
+        </div><!-- col-md-6 Finish -->
+        <div class="col-md-6"><!-- col-md-6 Begin -->
+            
+            
+        </div><!-- col-md-6 Finish -->
+    </div><!-- container Finish -->
+</div><!-- #copyright Finish -->
