ed553ee0148d265a91f64f2b0b2c2b46ba636abf
subinArakkalsureshbabu <subinarakkalsuresh@gmail.com>
2020-04-20 04:50:57 +0100

    Create frontpage.php

---
+++ frontpage.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta http-equiv="X-UA-Compatible" content="IE-edge" />
+	<meta name="viewport" content="width=device-width" />
+	<title>Home</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+	<link type="text/css" rel="stylesheet" href="css/bootstrap.css" />
+	
+</head>
+<body>
+	<script type="text/javascript" src="js/jquery-3.2.1.min.js"></script>
+	<script type="text/javascript" src="js/bootstrap.js"></script>
+	<div class="container-fluid">
+		<div class="container-fluid" style="background-color: black;color: white;">
+		<div class="container">
+	     <p class="pull-right">E-mail:Subinsuresh@example.com</p>
+	   </div>
+	   </div>
+		<div class="container">
+			<h1><font face="Centaur">VLUN</font></h1>
+		</div>
+		<div class="container">
+        </div>
+        </body>
+        </html>
