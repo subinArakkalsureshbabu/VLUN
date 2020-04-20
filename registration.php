0ea77ea30058c7feeb2c01db8905220302c48aaa
subinArakkalsureshbabu <subinarakkalsuresh@gmail.com>
2020-04-20 23:43:53 +0100

    Create registration.php

---
+++ registration.php
@@ -0,0 +1,23 @@
+<?php
+$Email = $_POST['user'];
+$Pass = $_POST['password'];
+
+
+//Database connection
+
+$conn = new mysqli('localhost','root','','vlu');
+if($conn->connect_error){
+die('Connection Failed : ');
+}
+else {
+ $stmt = $conn->prepare("insert into usertable Values(?,?)");
+ $stmt->bind_param("ss",$Email,$Pass);
+ $stmt->execute();
+ echo "registration Succssfully Completed......";
+    
+ $stmt->close();
+ $stmt->close();
+
+}
+
+?>
